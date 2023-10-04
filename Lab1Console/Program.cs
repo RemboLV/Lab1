3bb8f5a6731e8d0b8b4891a8eca2a5365d0ac409
RemboLV <elvis.igaunis@gmail.com>
2023-10-04 22:58:14 +0300

    Add project files.

---
+++ Lab1Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lab1Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var studData = new StudentsData();
+                studData.Load(StudentsData.DefaultFilename);
+                foreach (var stud in studData.Students)
+                    System.Console.WriteLine(stud);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+            System.Console.ReadLine();
+
+        }
+    }
+}
---
+++ Lab1Library/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Library
+{
+    [Serializable]
+    public class Student
+    {
+        /// <summary>
+        /// Studenta vārds.
+        /// </summary>
+        /* Vizuālā programmēšana, G. Alksnis, 2015, r162 */ /* 12 */
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Studenta uzvārds.
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Studenta apliecības numurs.
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// Konstruktors, kas nepieciešams atribūtam [Serializable]
+        /// </summary>
+        /// 
+        public string Group { get; set; }
+        /// <summary>
+        /// Studenta grupas numurs.
+        /// </summary>
+        /// 
+        public string Email { get; set; }
+        /// <summary>
+        /// Studenta epasts.
+        /// </summary>
+        /// 
+        private Student() { }
+        /// <summary>
+        /// Konstruktors, kas inicializē klases īpašības.
+        /// </summary>
+        public Student(string name, string surname, string id, string group, string email)
+        {
+            //ja kāds no parametriem ir tukša virkne, izraisīt izņēmumu
+            if (name.Length == 0 || surname.Length == 0 || id.Length == 0 || group.Length == 0 || email.Length == 0) throw new Exception("Invalid student data !");
+            this.Name = name;
+            this.Surname = surname;
+            this.Id = id;
+            this.Group = group;
+            this.Email = email;
+        }
+        /// <summary>
+        // Teksta virknes formā atgriež klases īpašību vērtības.
+        /// </summary>
+        public override string ToString()
+        {
+            return Id + " : " + Name + " " + Surname + " " + Group + " " + Email;
+        }
+
+    }
+}
